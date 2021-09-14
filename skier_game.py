cc4ec73fcf015048165ef4a9657abcb3c553b408
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2021-09-14 12:59:48 +0100

    added obstacles and obstacle class

--- skier_game.py
+++ skier_game.py
@@ -46,6 +46,49 @@ class SkierClass(pygame.sprite.Sprite):
             
             
             
+class ObstacleClass(pygame.sprite.Sprite):
+    def __init__(self, img_path, location, attribute):
+        pygame.sprite.Sprite.__init__(self)
+        self.img_path = img_path
+        self.image = pygame.image.load(self.img_path)
+        self.location = location
+        self.rect = self.image.get_rect()
+        self.rect.center = self.location
+        self.attribute = attribute
+        self.passed = False
+        
+    def move(self, num):
+        self.rect.centery = self.location[1] - num
+        
+def creatObstacles(s, e, num=10):
+    obstacles = pygame.sprite.Group()
+    locations = []
+    
+    for i in range(num):
+        row = random.randint(s, e)
+        col = random.randint(0, 9)
+        location = [col*64+20, row*64+20]
+        if location not in locations:
+            locations.append(location)
+            attribute = random.choice(list(cfg.OBSTACLE_PATHS.keys()))
+            img_path = cfg.OBSTACLE_PATHS[attribute]
+            obstacle = ObstacleClass(img_path, location, attribute)
+            obstacles.add(obstacle)
+    return obstacles 
+
+def AddObstacles(obstacles0, obstacles1):
+    obstacles = pygame.sprite.Group()
+    for obstacle in obstacles0:
+        obstacles.add(obstacle)
+    for obstacle in obstacles1:
+        obstacles.add(obstacle)
+    return obstacles
+            
+            
+            
+            
+    
+        
             
         
         
