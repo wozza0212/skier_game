f7c45763192f88c29f8f29d5ff238894501f97c7
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2021-09-14 12:42:28 +0100

    skier class finished

--- skier_game.py
+++ skier_game.py
@@ -19,5 +19,34 @@ class SkierClass(pygame.sprite.Sprite):
         self.rect.center = [320, 100]
         self.speed = [self.direction, 6 - abs(self.direction) * 2]
         
+    def turn(self, num):
+        self.direction += num
+        self.direction = max(-2, self.direction)
+        self.direction = min(2, self.direction)
+        
+        center = self.rect.center
+        self.image = pygame.image.load(self.imagepaths[self.direction])
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+        self.speed = [self.direction, 6-abs(self.direction)*2]
+        return self.speed
+    
+    def move(self):
+        self.rect.centerx += self.speed[0]
+        self.rect.centerx = max(20, self.rect.centerx)
+        self.rect.center = min(620, self.rect.centerx)
+        
+    
+    def setFall(self):
+        self.image = pygame.image.load(cfg.SKIER_IMAGE_PATHS[-1])
+        
+    def setForward(self):
+        self.direction = 0
+        self.image = pygame.image.load(self.imagepaths[self.direction])
+            
+            
+            
+            
+        
         
         
